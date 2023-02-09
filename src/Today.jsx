5c0c95c7e47f788a49b04502f3a075675a190e44
RishitSoni <115358770+RishitSoni@users.noreply.github.com>
2023-02-09 15:26:53 +0530

    first commit

---
+++ src/Today.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ListRender from './ListRender';
+import styles from './Today.module.css'
+
+export default function Today({ list, remove }) {
+    const date = new Date()
+    const filteredList = list.filter((task) => {
+        if (date.getFullYear() !== task.date.getFullYear()) {
+            return false
+        }
+        if (date.getMonth() !== task.date.getMonth()) {
+            return false;
+        }
+        if (date.getDate() !== task.date.getDate()) {
+            return false;
+        }
+        return true
+    })
+    return (
+        <div>
+            <ListRender filteredList={filteredList} remove={remove} />
+        </div>
+    )
+}
